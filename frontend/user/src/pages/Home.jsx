7527ec27bad0eb7e048dc3172273c8f18c764271
Shihab Uddin <infoshihab00000@gmail.com>
2025-09-02 18:52:21 +0600

    Update the home layout

--- frontend/user/src/pages/Home.jsx
+++ frontend/user/src/pages/Home.jsx
@@ -2,21 +2,47 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Banner } from "../components/Banner";
 import { About } from "../components/About";
+import { Service } from "../components/Service";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
-import { Service } from "../components/Service";
 
-function Home() {
+export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Banner id="banner" />
-      <About id="about" />
-      <Service id="service" />
-      <Contact id="contact" />
-      <Footer />
+    <div className="flex flex-col min-h-screen">
+      <header className="sticky top-0 z-50">
+        <Navbar />
+      </header>
+
+      <main className="flex flex-col flex-1">
+        <section id="banner" className="w-full">
+          <Banner />
+        </section>
+
+        <section
+          id="about"
+          className="w-full py-16 md:py-24 px-4 md:px-12 bg-gray-50"
+        >
+          <About />
+        </section>
+
+        <section
+          id="service"
+          className="w-full py-16 md:py-24 px-4 md:px-12 bg-white"
+        >
+          <Service />
+        </section>
+
+        <section
+          id="contact"
+          className="w-full py-16 md:py-24 px-4 md:px-12 bg-gray-50"
+        >
+          <Contact />
+        </section>
+      </main>
+
+      <footer className="w-full">
+        <Footer />
+      </footer>
     </div>
   );
 }
-
-export default Home;
