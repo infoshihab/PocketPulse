916d2d7bc5c5f1f771269e1facd81b23b44a23e6
Shihab Uddin <infoshihab00000@gmail.com>
2025-09-05 00:05:32 +0600

    setting some changes

--- backend/server.js
+++ backend/server.js
@@ -21,19 +21,16 @@ mongoose
   });
 
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://pocketpulsefrontend.onrender.com",
+  "http://localhost:5173",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
   })
